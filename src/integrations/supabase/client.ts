12940eb293f853ec82e735ef00f9024d03005a48
braxtonfrancom <83977454+braxtonfrancom@users.noreply.github.com>
2026-03-17 16:07:55 -0500

    Update client.ts
    Changed naming of key to anon_key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,15 +3,15 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const VITE_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 console.log("URL:", SUPABASE_URL);
-console.log("KEY:", SUPABASE_PUBLISHABLE_KEY);
+console.log("KEY:", VITE_SUPABASE_ANON_KEY);
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, VITE_SUPABASE_ANON_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
